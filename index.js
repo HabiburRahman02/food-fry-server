4d5ddd84571778a36077e871c6c60a64b108fa91
Habibur Rahman <habibur88454@gmail.com>
2025-01-05 19:19:04 +0600

    delete user using specific id

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/user/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // cart related apis
         app.get('/cart', async (req, res) => {
             const email = req.query.email;
